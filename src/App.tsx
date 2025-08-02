45dd8e90f2e0bb35f1f9080d59449af07df0808f
Mohammed-Asif04 <mohad.asif0407@gmail.com>
2025-08-02 21:43:06 +0530

    Update

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom"
+import "./App.css"
+import { Button } from "./components/ui/button"
+function App() {
+  
+
+  return (
+   <BrowserRouter>
+   
+   </BrowserRouter>
+   
+  )
+}
+
+export default App
