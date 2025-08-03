c929aaab273bb459ba8e53d29cff66126b3dd278
Mohammed-Asif04 <mohad.asif0407@gmail.com>
2025-08-03 12:44:32 +0530

    Update

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,20 @@
 import type { PropsWithChildren } from "react";
+import Header from "./header";
 
-const layout = ({ children }: PropsWithChildren) => {
+const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="bg-gradient-to-br from-background to-muted">
-      header
-    <main className="min-h-screen conatiner mx-auto px-4 py-8">
-        {children}</main>
-      footer
+    <div className=" bg-gradient-to-br from-background to-muted">
+      <Header />
+      <main className="min-h-screen container mx-auto px-4 py-8">
+        {children}
+      </main>
+      <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
+        <div className="container mx-auto px-4 text-center text-gray-200">
+          <p>Made with 💗 by Asif-Coder</p>
+        </div>
+      </footer>
     </div>
   );
 };
 
-export default layout;
+export default Layout;
